65c89fec03fe8760e71700ea91cdd01f1d2d280f
jerome papazian <j_papazian2000@yahoo.fr>
2025-01-16 09:41:34 +0100

    add output revokation v2

--- workspace_repo/outputs.tf
+++ workspace_repo/outputs.tf
@@ -1,6 +1,7 @@
 output "vm_ip" {
-    value = module.gcp-infra.vm_ip
+    value = tos(module.gcp-infra.vm_ip)
 }
+
 output "artifact_revocation_date" {
   value = data.hcp_packer_artifact.apache_gce.revoke_at
 }
